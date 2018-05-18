442ff59627f9dda1abf836f660b9916b5c4db59a
Unknown <drmcmurderface2@mail.com>
2018-05-18 03:58:12 -0500

    Added tests, edited Copy

--- TM_CommonPy/__init__.py
+++ TM_CommonPy/__init__.py
@@ -3,6 +3,7 @@ import io
 import importlib
 import pip
 import xml.etree.ElementTree
+import shutil
 
 #Maybe use __file__ instead?
 def GetScriptRoot():
@@ -21,23 +22,28 @@ def GetXMLNamespaces(sXMLFile):
     ])
     return cNamespaces
 
+def Copy(src,root_dst_dir):
+    if os.path.isdir(src):
+        for src_dir, dirs, files in os.walk(src):
+            dst_dir = src_dir.replace(src, root_dst_dir, 1)
+            if not os.path.exists(dst_dir):
+                os.makedirs(dst_dir)
+            for file_ in files:
+                src_file = os.path.join(src_dir, file_)
+                dst_file = os.path.join(dst_dir, file_)
+                if os.path.exists(dst_file):
+                    os.remove(dst_file)
+                shutil.copy(src_file, dst_dir)
+    elif os.path.isfile(src):
+        dst_dir = src_dir.replace(src, root_dst_dir, 1)
+        shutil.copy(src, dst_dir)
+    else:
+        print("Copy\Error\src "+src+" is not a valid file or directory")
+
 #not tested
 def TryMakeDirs(sDir):
     if not os.path.exists(sDir):
         os.makedirs(sDir)
-
-#not tested
-def copyanything(root_src_dir,root_dst_dir):
-    for src_dir, dirs, files in os.walk(root_src_dir):
-        dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
-        if not os.path.exists(dst_dir):
-            os.makedirs(dst_dir)
-        for file_ in files:
-            src_file = os.path.join(src_dir, file_)
-            dst_file = os.path.join(dst_dir, file_)
-            if os.path.exists(dst_file):
-                os.remove(dst_file)
-            shutil.copy(src_file, dst_dir)
 
 #not tested
 def InstallAndImport(package):
--- TM_CommonPy/tests/test_TM_CommonPy.py
+++ TM_CommonPy/tests/test_TM_CommonPy.py
@@ -1,24 +1,66 @@
 from unittest import TestCase
 import os
+import shutil
+import sys
 
-import TM_CommonPy as TMCP
+
+def __Copy(src,root_dst_dir):
+    if os.path.isdir(src):
+        for src_dir, dirs, files in os.walk(src):
+            dst_dir = src_dir.replace(src, root_dst_dir, 1)
+            if not os.path.exists(dst_dir):
+                os.makedirs(dst_dir)
+            for file_ in files:
+                src_file = os.path.join(src_dir, file_)
+                dst_file = os.path.join(dst_dir, file_)
+                if os.path.exists(dst_file):
+                    os.remove(dst_file)
+                shutil.copy(src_file, dst_dir)
+    elif os.path.isfile(src):
+        dst_dir = src_dir.replace(src, root_dst_dir, 1)
+        shutil.copy(src, dst_dir)
+    else:
+        print("__Copy\\Error\\src is not a valid file or directory")
+__Copy('Examples_Backup','Examples')
+
+try:
+    import TM_CommonPy as TMC
+except:
+    sys.path.append(os.path.join(__file__,'..','..','..'))
+    import TM_CommonPy as TMC
+
+
 
 class Test_TM_CommonPy(TestCase):
-    sExampleXMLFile = os.path.join('TM_CommonPy','tests','ExampleXML.xml')
-    sExampleTXTFile = os.path.join('TM_CommonPy','tests','ExampleTXT.txt')
+    #?where to chdir?
+    os.chdir(os.path.join('TM_CommonPy','tests'))
+    sExampleXMLFile = os.path.join('Examples','ExampleXML.xml')
+    sExampleTXTFile = os.path.join('Examples','ExampleTXT.txt')
 
     def test_GetScriptRoot_IsString(self):
-        s = TMCP.GetScriptRoot()
+        s = TMC.GetScriptRoot()
         self.assertTrue(isinstance(s, str))
 
     def test_GetFileContent_IsString(self):
-        s = TMCP.GetFileContent(self.sExampleXMLFile)
+        s = TMC.GetFileContent(self.sExampleXMLFile)
         self.assertTrue(isinstance(s, str))
 
     def test_GetFileContent_ByExample(self):
-        b = TMCP.GetFileContent(self.sExampleTXTFile) == "I am example txt.\nHear me. Or, read me, rather."
+        b = TMC.GetFileContent(self.sExampleTXTFile) == "I am example txt.\nHear me. Or, read me, rather."
         self.assertTrue(b)
 
+    def test_GetFileContent_ByExample2(self):
+        b = TMC.GetFileContent(self.sExampleTXTFile) == "I am example txt.\nHear me. Or, read me, maybe."
+        self.assertTrue(not b)
+
     def test_GetXMLNamespaces_ByExample(self):
-        b = str(TMCP.GetXMLNamespaces(self.sExampleXMLFile)) == '{\'\': \'http://schemas.microsoft.com/developer/msbuild/2003\'}'
+        b = str(TMC.GetXMLNamespaces(self.sExampleXMLFile)) == '{\'\': \'http://schemas.microsoft.com/developer/msbuild/2003\'}'
+        self.assertTrue(b)
+
+    def test_Copy_ByExample(self):
+        TMC.Copy('Examples','Examples_TmpCopy')
+        b = False
+        if os.path.isfile(os.path.join('Examples_TmpCopy','ExampleXML.xml')) and os.path.isfile(os.path.join('Examples_TmpCopy','ExampleTXT.txt')):
+            b = True
+        shutil.rmtree('Examples_TmpCopy')
         self.assertTrue(b)
