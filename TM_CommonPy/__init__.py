28b8c8207c84690d7c17bef58a97dd13a2aebe35
Unknown <drmcmurderface2@mail.com>
2018-06-07 04:04:15 -0500

    fragile class

--- TM_CommonPy/__init__.py
+++ TM_CommonPy/__init__.py
@@ -202,6 +202,23 @@ def ListFiles(sDir):
     return sReturning
 
 #dev
+class fragile(object):
+    class Break(Exception):
+      """Break out of the with statement"""
+
+    def __init__(self, value):
+        self.value = value
+
+    def __enter__(self):
+        return self.value.__enter__()
+
+    def __exit__(self, etype, value, traceback):
+        error = self.value.__exit__(etype, value, traceback)
+        if etype == self.Break:
+            return True
+        return error
+
+#dev
 def InstallAndImport(package):
     try:
         importlib.import_module(package)
