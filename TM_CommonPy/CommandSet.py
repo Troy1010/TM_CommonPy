955fc8104cb7aeb1851ff5cb2a00a59e07211842
Unknown <drmcmurderface2@mail.com>
2018-06-13 15:31:09 -0500

    Module Restructuring

---
+++ TM_CommonPy/CommandSet.py
@@ -0,0 +1,49 @@
+import os, sys
+import importlib
+import pip
+import xml.etree.ElementTree
+import shutil
+import subprocess
+import shlex
+import stat
+import importlib
+import pkgutil
+import inspect
+import importlib.util
+import TM_CommonPy.Narrator
+import ctypes
+import TM_CommonPy as TM
+
+#beta
+class CommandSet:
+    def __init__(self):
+        self.PreviousCommandSet = []
+        self.CommandSet = []
+    def Que(self,cDoUndoPair,cArgs):
+        if not TM.IsCollection(cArgs):
+            cArgs = [cArgs]
+        if len(cDoUndoPair) != 2:
+            raise ValueError(self.__class__.__name__+"::"+TM.FnName()+": first arg must be a container of 2 methods: Do and Undo")
+        self.CommandSet.append([cDoUndoPair,cArgs])
+    def Execute(self,bRedo=False):
+        #---Undo what is in PreviousCommandSet but not CommandSet
+        for vItem in [x for x in self.PreviousCommandSet if x not in self.CommandSet]:
+            print ("Undo:"+str(vItem[0]))
+            self._Undo(vItem[0],vItem[1])
+        if bRedo:
+            #---Do what is in CommandSet
+            for vItem in self.CommandSet:
+                print ("ReDo:"+str(vItem[0]))
+                self._Do(vItem[0],vItem[1])
+        else:
+            #---Do what is in CommandSet but not PreviousCommandSet
+            for vItem in [x for x in self.CommandSet if x not in self.PreviousCommandSet]:
+                print ("Do:"+str(vItem[0]))
+                self._Do(vItem[0],vItem[1])
+        #---
+        self.PreviousCommandSet = self.CommandSet
+        self.CommandSet = []
+    def _Do(self,cDoUndoPair,cArgs):
+        cDoUndoPair[0](*cArgs)
+    def _Undo(self,cDoUndoPair,cArgs):
+        cDoUndoPair[1](*cArgs)
