fa419ec08230313bd7583259337a41c1a6ed536b
Unknown <drmcmurderface2@mail.com>
2018-06-16 18:46:17 -0500

    Logging

--- TM_CommonPy/__init__.py
+++ TM_CommonPy/__init__.py
@@ -4,18 +4,21 @@ bWriteLog=True
 
 __version__ = '0.10.0'
 #__all__ = ["CommandSet"]
+##region LogInit
+import logging, os
+TMLog = logging.getLogger('TM_CommonPy')
+if bWriteLog:
+    sLogFile = os.path.join(__file__,'..','TMLog.log')
+    if os.path.exists(sLogFile):
+        os.remove(sLogFile)
+    TMLog.addHandler(logging.FileHandler(sLogFile))
+##endregion
 ##region ImportThisModule
 import TM_CommonPy as TM
 from TM_CommonPy.Misc import *
 from TM_CommonPy.CommandSet import CommandSet
 from TM_CommonPy.CopyContext import CopyContext
 from TM_CommonPy.ElementTreeContext import ElementTreeContext
-##endregion
-##region Log init
-import logging, os
-TMLog = logging.getLogger('TM_CommonPy')
-if bWriteLog:
-    sLogFile = os.path.join(__file__,'..','TMLog.log')
-    TM.Delete(sLogFile)
-    TMLog.addHandler(logging.FileHandler(sLogFile))
+import TM_CommonPy.Narrator
+import TM_CommonPy.Conan
 ##endregion
