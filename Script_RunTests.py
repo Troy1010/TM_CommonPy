8e206f84f298c2f9c9853552810121cbec581335
Troy1010 <drmcmurderface2@mail.com>
2018-12-05 06:10:05 -0600

    CommandSet-Linted

--- Script_RunTests.py
+++ Script_RunTests.py
@@ -1,2 +1,2 @@
 import TM_CommonPy as TM
-TM.devtools.RunTests(sTestPath='TM_CommonPy/_tests',bPause=True)
+TM.devtools.RunTests(sTestPath='TM_CommonPy/_tests', bPause=False)
--- TM_CommonPy/CommandSet.py
+++ TM_CommonPy/CommandSet.py
@@ -6,78 +6,98 @@ import dill
 from enum import Enum
 from types import ModuleType
 
-#I can't put this into CommandSet or it becomes unpickleable.
-#CommandSet_QueType = Enum("CommandSet_QueType","Function Script")
+
 class CommandSet_QueType(Enum):
+    # I can't put this into CommandSet or it becomes unpickleable.
+    # CommandSet_QueType = Enum("CommandSet_QueType","Function Script")
     Function = 1
     Script = 2
+
+
 class CommandSet:
     def __init__(self):
         self.PreviousCommandSet = []
         self.CommandSet = []
-    def Que(self,cDoUndoPair,cArgs):
-        #---Filter
+
+    def Que(self, cDoUndoPair, cArgs):
+        # ---Filter
         if len(cDoUndoPair) != 2:
-            raise ValueError(self.__class__.__name__+"::"+TM.FnName()+"`first arg must be a container of 2 methods: Do and Undo")
+            raise ValueError(self.__class__.__name__ + "::" + TM.FnName()
+                             + "`first arg must be a container of 2 methods: Do and Undo")
         if not TM.IsCollection(cArgs):
             cArgs = [cArgs]
-        #---
-        self.CommandSet.append([CommandSet_QueType.Function,cDoUndoPair,cArgs])
-    def QueScript(self,sFilePath,cArgs):
-        #---Filter
+        # ---
+        self.CommandSet.append(
+            [CommandSet_QueType.Function, cDoUndoPair, cArgs])
+
+    def QueScript(self, sFilePath, cArgs):
+        # ---Filter
         if not os.path.isfile(sFilePath):
-            raise FileNotFoundError("sFilePath is not a file:"+sFilePath)
-        #---Determine sScript
+            raise FileNotFoundError("sFilePath is not a file:" + sFilePath)
+        # ---Determine sScript
         sScript = open(sFilePath).read()
-        #---Make sure Script has Do and Undo
-        vModule = ModuleType("CommandSetQuedScript", "This module represents a qued script")
+        # ---Make sure Script has Do and Undo
+        vModule = ModuleType("CommandSetQuedScript",
+                             "This module represents a qued script")
         exec(sScript, vModule.__dict__)
-        if not (hasattr(vModule,"Do") and hasattr(vModule,"Undo")):
-            raise Exception("QueScript`Script must have Do and Undo functions.")
-        #---
-        self.CommandSet.append([CommandSet_QueType.Script,sScript,cArgs])
-    def Execute(self,bRedo=False):
-        TMLog.debug(self.__class__.__name__+"::"+TM.FnName()+"`Open")
+        if not (hasattr(vModule, "Do") and hasattr(vModule, "Undo")):
+            raise Exception(
+                "QueScript`Script must have Do and Undo functions.")
+        # ---
+        self.CommandSet.append([CommandSet_QueType.Script, sScript, cArgs])
+
+    def Execute(self, bRedo=False):
+        TMLog.debug(self.__class__.__name__ + "::" + TM.FnName() + "`Open")
         if bRedo:
-            #---Undo what is in PreviousCommandSet
+            # ---Undo what is in PreviousCommandSet
             for vItem in self.PreviousCommandSet:
-                TMLog.debug(self.__class__.__name__+"::"+TM.FnName()+"`Reundo:"+str(vItem[0]))
+                TMLog.debug(self.__class__.__name__ + "::"
+                            + TM.FnName() + "`Reundo:" + str(vItem[0]))
                 self._Undo(*vItem)
         else:
-            #---Undo what is in PreviousCommandSet but not CommandSet
+            # ---Undo what is in PreviousCommandSet but not CommandSet
             for vItem in [x for x in self.PreviousCommandSet if x not in self.CommandSet]:
-                TMLog.debug(self.__class__.__name__+"::"+TM.FnName()+"`Undo:"+str(vItem[0]))
+                TMLog.debug(self.__class__.__name__ + "::"
+                            + TM.FnName() + "`Undo:" + str(vItem[0]))
                 self._Undo(*vItem)
         if bRedo:
-            #---Do what is in CommandSet
+            # ---Do what is in CommandSet
             for vItem in self.CommandSet:
-                TMLog.debug(self.__class__.__name__+"::"+TM.FnName()+"`Redo:"+str(vItem[0]))
+                TMLog.debug(self.__class__.__name__ + "::"
+                            + TM.FnName() + "`Redo:" + str(vItem[0]))
                 self._Do(*vItem)
         else:
-            #---Do what is in CommandSet but not PreviousCommandSet
+            # ---Do what is in CommandSet but not PreviousCommandSet
             for vItem in [x for x in self.CommandSet if x not in self.PreviousCommandSet]:
-                TMLog.debug(self.__class__.__name__+"::"+TM.FnName()+"`Do:"+str(vItem[0]))
+                TMLog.debug(self.__class__.__name__ + "::"
+                            + TM.FnName() + "`Do:" + str(vItem[0]))
                 self._Do(*vItem)
-        #---
+        # ---
         self.PreviousCommandSet = self.CommandSet
         self.CommandSet = []
+
     def Save(self):
         with open('CommandSet.pickle', 'wb') as handle:
             dill.dump(self, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    def _Do(self,eQueType,vAction,cArgs):
+
+    def _Do(self, eQueType, vAction, cArgs):
         if eQueType == CommandSet_QueType.Function:
             vAction[0](*cArgs)
         elif eQueType == CommandSet_QueType.Script:
-            vModule = ModuleType("CommandSetQuedScript", "This module represents a qued script")
+            vModule = ModuleType("CommandSetQuedScript",
+                                 "This module represents a qued script")
             exec(vAction, vModule.__dict__)
             vModule.Do(*cArgs)
-    def _Undo(self,eQueType,vAction,cArgs):
+
+    def _Undo(self, eQueType, vAction, cArgs):
         if eQueType == CommandSet_QueType.Function:
             vAction[1](*cArgs)
         elif eQueType == CommandSet_QueType.Script:
-            vModule = ModuleType("CommandSetQuedScript", "This module represents a qued script")
+            vModule = ModuleType("CommandSetQuedScript",
+                                 "This module represents a qued script")
             exec(vAction, vModule.__dict__)
             vModule.Undo(*cArgs)
+
     @staticmethod
     def TryLoad():
         try:
