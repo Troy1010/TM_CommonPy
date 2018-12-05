576dbf36ca3c3fb35296ed4eaf178b06cdd9bd9a
Unknown <drmcmurderface2@mail.com>
2018-12-04 23:57:58 -0600

    WorkspaceContext-Linted

--- TM_CommonPy/WorkspaceContext.py
+++ TM_CommonPy/WorkspaceContext.py
@@ -1,25 +1,27 @@
 import TM_CommonPy as TM
 import os
 
-#beta
+
 class WorkspaceContext:
-    def __init__(self,sPath,sSource=None,bPostDelete=False,bCDInto=True,bPreDelete=False):
+    def __init__(self, sPath, sSource=None, bPostDelete=False, bCDInto=True, bPreDelete=False):
         self.bCDInto = bCDInto
         self.bPostDelete = bPostDelete
         self.sPath = sPath
         self.sSource = sSource
         self.bPreDelete = bPreDelete
+
     def __enter__(self):
         if self.bPreDelete:
             TM.Delete(self.sPath)
         if self.sSource is None:
             TM.TryMkdir(self.sPath)
         else:
-            TM.Copy(self.sSource,self.sPath)
+            TM.Copy(self.sSource, self.sPath)
         if self.bCDInto:
             self.OldCWD = os.getcwd()
             os.chdir(self.sPath)
-    def __exit__(self,errtype,value,traceback):
+
+    def __exit__(self, errtype, value, traceback):
         if self.bCDInto:
             os.chdir(self.OldCWD)
         if self.bPostDelete:
