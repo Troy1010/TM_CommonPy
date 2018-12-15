d96da9b46c089b39079ec89274dd82f609c5cb72
Unknown <drmcmurderface2@mail.com>
2018-12-15 16:38:53 -0600

    minor

--- Script_RunTests_Conditional.py
+++ Script_RunTests_Conditional.py
@@ -1,2 +1,2 @@
 import TM_CommonPy as TM
-TM.devtools.RunTests(sTestPath='TM_CommonPy/_tests', bPause=True, sEval='(count < 13 and TM_CommonPy_Test)')
+TM.devtools.RunTests(sTestPath='TM_CommonPy/_tests', bPause=False, sEval='(count < 1 and TM_CommonPy_Test)', iVerbosity=2)
