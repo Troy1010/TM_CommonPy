4569b40349336e5a2697e4981013e988e8ae4fcf
Troy1010 <drmcmurderface2@mail.com>
2019-01-15 22:13:55 -0600

    tk-FocusNextWritableCell

--- TM_CommonPy/tk/TableFrame.py
+++ TM_CommonPy/tk/TableFrame.py
@@ -1,19 +1,54 @@
 import tkinter as tk
-import TM_CommonPy as TM
 from . import Constants
 from TM_CommonPy._Logger import TMLog
 
 
 class TableFrame(tk.Frame):
-    def Hello():
-        TMLog.debug("Hello")
-
     def GetCell(self, row, column):
-        return self.grid_slaves(row, column)[0]
+        try:
+            return self.grid_slaves(row, column)[0]
+        except IndexError:
+            return None
 
     def FocusNextWritableCell(self, cellToSearchFrom=None, searchDirection=Constants.HORIZONAL):
-        TMLog.debug("FocusNextWritableCell`Open")
-        if searchDirection == Constants.HORIZONAL:
-            TMLog.debug("horiz")
+        if cellToSearchFrom:
+            firstRow = cellToSearchFrom.row
+            firstCol = cellToSearchFrom.column
         else:
-            TMLog.debug("vert")
+            firstRow = cellToSearchFrom.row
+            firstCol = cellToSearchFrom.column
+        if searchDirection == Constants.HORIZONAL:
+            curCol = firstCol + 1
+            curRow = firstRow
+        else:
+            curCol = firstCol
+            curRow = firstRow + 1
+
+        while not (curCol == firstCol and curRow == firstRow):
+            cell = self.GetCell(curRow, curCol)
+            if "entry" in str(type(cell)).lower() and cell.cget('state') == tk.NORMAL:
+                cell.focus_set()
+                return
+            if searchDirection == Constants.HORIZONAL:
+                if self.GetCell(curRow, curCol + 1):
+                    curCol += 1
+                elif self.GetCell(curRow + 1, 0):
+                    curCol = 0
+                    curRow += 1
+                elif self.GetCell(0, 0):
+                    curCol = 0
+                    curRow = 0
+                else:
+                    break
+            else:
+                if self.GetCell(curRow + 1, curCol):
+                    curRow += 1
+                elif self.GetCell(0, curCol + 1):
+                    curCol += 1
+                    curRow = 0
+                elif self.GetCell(0, 0):
+                    curCol = 0
+                    curRow = 0
+                else:
+                    break
+        self.winfo_toplevel().focus_set()
