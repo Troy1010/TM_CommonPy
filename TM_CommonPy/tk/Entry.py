429b070331cc2e93848ba299bd029d5c71d3e391
Troy1010 <drmcmurderface2@mail.com>
2019-01-29 00:13:35 -0600

    tkinter-Entry-AddDisposable, ValidationHandler-bugfix

--- TM_CommonPy/tk/Entry.py
+++ TM_CommonPy/tk/Entry.py
@@ -2,11 +2,22 @@ import tkinter as tk
 
 
 class Entry(tk.Entry):
-    ValidationHandler = None
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cDisposables = []
+        self.ValidationHandler = None
 
     def MakeValid(self):
         """ Useful if text is changed outside of its setter """
         self.text = self.text
+
+    def AddDisposable(self, disposable):
+        self.cDisposables.append(disposable)
+
+    def destroy(self):
+        for disposable in self.cDisposables:
+            disposable.dispose()
+        super().destroy()
 
     @property
     def column(self):
