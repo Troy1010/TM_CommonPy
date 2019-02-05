418f7600248d621c7c98c47f16a02a341be20d7e
Troy1010 <drmcmurderface2@mail.com>
2019-02-05 17:49:13 -0600

    Entry- Entry_DirectStream

--- TM_CommonPy/tk/Entry.py
+++ TM_CommonPy/tk/Entry.py
@@ -3,6 +3,61 @@ from .Cell_Inheritable import Cell_Inheritable
 import rx
 
 
+class Entry_DirectStream(Cell_Inheritable, tk.Entry):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # make FocusOut an observable
+        self.FocusOut_stream = rx.subjects.Subject()
+        self.bind("<FocusOut>", lambda event: self.FocusOut_stream.on_next(None), add="+")
+        #
+        self.DisplayHandler = lambda x: x
+        # trigger text setter
+        if 'text' in kwargs:
+            self.text = kwargs['text']
+
+    @property
+    def text(self):
+        return self.get()
+
+    @text.setter
+    def text(self, value):
+        # text
+        if isinstance(value, rx.Observable):
+            self.stream = value
+            self.DisposeDisposables()
+            # when stream changes, change my text
+
+            def AssignText(value):
+                self.text = value
+            self.cDisposables.append(self.stream.subscribe(AssignText))
+            # when my text changes, change stream
+
+            def OnFocusOut(nothing):
+                value = self.ValidationHandler(self.text) if self.ValidationHandler is not None else self.text
+                self.stream.on_next(value)
+            self.cDisposables.append(self.FocusOut_stream.subscribe(OnFocusOut))
+            #
+            return
+        #
+        state = self["state"]
+        self.configure(state='normal')
+        self.delete(0, tk.END)
+        if self.ValidationHandler is not None:
+            value = self.ValidationHandler(value)
+        value = self.DisplayHandler(value)
+        if value is not None:
+            self.insert(0, value)
+        self.configure(state=state)
+
+    def select_text(self):
+        self.select_range(0, 'end')
+        self.icursor('end')
+
+    def FocusNothing(self):
+        self.winfo_toplevel().focus_set()
+
+
 class Entry(Cell_Inheritable, tk.Entry):
 
     def __init__(self, *args, **kwargs):
@@ -25,18 +80,6 @@ class Entry(Cell_Inheritable, tk.Entry):
             def AssignText(value):
                 self.text = value
             self.cDisposables.append(value.subscribe(AssignText))
-            # when entry text changes, change stream
-
-            def OnFocusOut_FeedToStream(event):
-                if not hasattr(self, '__temp_obs'):
-                    self.__temp_obs = rx.subjects.Subject()
-                    self.cDisposables.append(self.__temp_obs.distinct_until_changed().subscribe(value))
-                if self.ValidationHandler:
-                    self.__temp_obs.on_next(self.ValidationHandler(self.get()))
-                else:
-                    self.__temp_obs.on_next(self.get())
-            self.bind("<FocusOut>", OnFocusOut_FeedToStream, add="+")
-            self.bind("<FocusOut>", lambda event: print("focus out"), add="+")
             return
         #
         state = self["state"]
--- TM_CommonPy/tk/__init__.py
+++ TM_CommonPy/tk/__init__.py
@@ -1,7 +1,8 @@
 from .TableFrame import TableFrame
 from .Entry import Entry
+from .Entry import Entry_DirectStream
 from .Button import Button
 from .Constants import HORIZONAL
 from .Constants import VERTICLE
 
-__all__ = ['TableFrame', 'Entry', 'Button', 'HORIZONAL', 'VERTICLE']
+__all__ = ['TableFrame', 'Entry', 'Button', 'HORIZONAL', 'VERTICLE', 'Entry_DirectStream']
